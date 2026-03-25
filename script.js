037bba97fd9356fd4222fdb51319022248e2f39e
I1HN <miguel.toulouzas@gmail.com>
2026-03-25 08:46:36 -0300

    Código da divisão

---
+++ script.js
@@ -0,0 +1,12 @@
+let divNum1;
+let divNum2;
+let divResul;
+//let dita o nome de uma variável;
+
+divNum1 = Number(prompt("Insira o primeiro número que será dividido"));
+divNum2 = Number(prompt("Insira o segundo número que vai dividir"));
+divResul = divNum1 / divNum2;
+//Number prompt faz a máquina ver os caracteres não como um texto e sim com número;
+
+alert(`O resultado dessa divisão é ${divResul}`);
+//alert exibe uma frase na tela;
