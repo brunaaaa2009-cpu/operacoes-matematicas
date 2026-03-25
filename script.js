8fc7296a27937e65bd1d420c8e4335e49ae76f90
I1HN <brunaaaa2009@gmail.com>
2026-03-25 08:51:16 -0300

    foi feito o comando para multiplicação.

---
+++ script.js
@@ -0,0 +1,12 @@
+let numUm;
+let numDois;
+let result;
+
+// let declara uma variável.
+// Number serve para o caracter ser interpretado como um número.
+// prompt abre uma caixa de texto para o usuário digitar.
+numUm = Number(prompt("digite o primeiro número"));
+numDois = Number(prompt("digite o segundo número"));
+result = numUm * numDois;
+alert(`O resultado da multiplicação é ${result}`);
+// alert exibe uma mensagem na tela.
