79f71a5ab9162bda3cac0f16e8f933e5f51e61e2
I1HN <gabrielleolimpio15@gmail.com>
2026-03-25 08:55:01 -0300

    fiz o comando para subtração

---
+++ script.js
@@ -0,0 +1,12 @@
+let numUm;
+let numDois;
+let resultado;
+//let declara uma váriavel 
+//Number serve para o caracter ser interpretado com um número
+// prompt abre uma caixa de texto para o usuário digitar
+numUm = Number(prompt("Digite o primeiro numero"));
+numDois = Number(prompt("Digite o segundo numero"));
+resultado = numUm - numDois
+alert(`A subtrção dos Dois números é ${resultado.toFixed(2)}`);
+//alert exibe uma mensagem na tela  
+
